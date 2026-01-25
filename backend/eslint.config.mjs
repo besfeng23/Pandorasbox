3cd185957078c43510d13e69ef644b4fbcd49a89
besfeng23 <joven_ong@hotmail.com>
2026-01-25 18:52:15 +0000

    Auto-commit: 2026-01-25 18:52:15

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,18 +1,40 @@
-import FlatCompat from "@eslint/compat";
-import path from "path";
-import { fileURLToPath } from "url";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import nextPlugin from "@next/eslint-plugin-next";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: path.dirname(__dirname),
-  resolvePluginsRelativeTo: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
-  ...compat.extends("next/typescript"),
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,tsx}"] },
+  {
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  {
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+      // Custom rules or overrides can go here
+    },
+  },
   {
     ignores: [
       "node_modules/**",
@@ -23,5 +45,3 @@ const eslintConfig = [
     ],
   },
 ];
-
-export default eslintConfig;
